2d4401c18d4fc20ac01f8862f65572a485bb536c
sansarun <sansarun@gmail.com>
2014-03-25 00:39:21 +0700

    ignore subcomment when count maincomment

--- src/TopicParser.js
+++ src/TopicParser.js
@@ -2,8 +2,8 @@ function TopicParser(html) {
 
 	this.dom = $(html);
 
-	this.getCommentNumber = function() {
-		return this.dom.find(".section-comment").length;
+	this.getMainCommentNumber = function() {
+		return this.dom.find(".section-comment:not(.sub-comment)").length;
 	};
 
 }
--- test/TopicParserSpec.js
+++ test/TopicParserSpec.js
@@ -1,14 +1,25 @@
 describe("TopicParser", function() {
 
-  	it("can count number of comment in topic", function() {
-  		var simpleTopicHtml = $.ajax({
+  	it("can count number of maincomment in topic", function() {
+  		var html = $.ajax({
 	        type: "GET",
 	        url: "base/test/html/simpleTopic.html",
 	        async: false
 	    }).responseText;
 
-  		var parser = new TopicParser(simpleTopicHtml);
-	    expect(parser.getCommentNumber()).toBe(4);
+  		var parser = new TopicParser(html);
+	    expect(parser.getMainCommentNumber()).toBe(4);
+  	});
+
+  	it("can count number of maincomment in topic that contain subcomment", function() {
+  		var html = $.ajax({
+	        type: "GET",
+	        url: "base/test/html/subcommentTopic.html",
+	        async: false
+	    }).responseText;
+
+  		var parser = new TopicParser(html);
+	    expect(parser.getMainCommentNumber()).toBe(8);
   	});
 
 });
